ccb9543282eff5c7284646edfbc9d6afff95f466
shinharad <shinharad.github@gmail.com>
2019-02-07 18:05:40 +0900

    add eval and writer monad

---
+++ src/main/scala/com/github/shinharad/cats/ch04/eval_monad/SaferFoldingUsingEval.scala
@@ -0,0 +1,31 @@
+package com.github.shinharad.cats.ch04.eval_monad
+
+import cats.Eval
+
+object SaferFoldingUsingEval extends App {
+
+  //  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
+  //    as match {
+  //      case head :: tail =>
+  //        fn(head, foldRight(tail, acc)(fn))
+  //      case Nil =>
+  //        acc
+  //    }
+
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
+    foldRightEval(as, Eval.now(acc)) { (a, b) =>
+      b.map(fn(a, _))
+    }.value
+
+  def foldRightEval[A, B](as: List[A], acc: Eval[B])(fn: (A, Eval[B]) => Eval[B]): Eval[B] =
+    as match {
+      case head :: tail =>
+        Eval.defer(fn(head, foldRightEval(tail, acc)(fn)))
+      case Nil =>
+        acc
+    }
+
+  val r = foldRight((1 to 100000).toList, 0L)(_ + _)
+  println(r)
+
+}
---
+++ src/main/scala/com/github/shinharad/cats/ch04/writer_monad/CreatingAndUnpackingWriters.scala
@@ -0,0 +1,42 @@
+package com.github.shinharad.cats.ch04.writer_monad
+
+import cats.data.Writer
+import cats.instances.vector._ // for Monoid
+import cats.syntax.applicative._ // for pure
+import cats.syntax.writer._ // for tell
+
+object CreatingAndUnpackingWriters extends App {
+
+  val writer = Writer(Vector(
+    "It was the best of times",
+    "it was the worst of times"
+  ), 1859)
+  println(writer) // => WriterT((Vector(It was the best of times, it was the worst of times),1859))
+
+  // Notice that the type reported on the console is actually WriterT[Id, Vector[String], Int] instead of Writer[Vector[String], Int] as we might expect.
+
+  type Logged[A] = Writer[Vector[String], A]
+
+  val r = 123.pure[Logged]
+  println(r) // => WriterT((Vector(),123))
+
+  val r2 = Vector("msg1", "msg2", "msg3").tell
+  println(r2) // => WriterT((Vector(msg1, msg2, msg3),()))
+
+  val a = Writer(Vector("msg1", "msg2", "msg3"), 123)
+  println(a) // => WriterT((Vector(msg1, msg2, msg3),123))
+
+  val b = 123.writer(Vector("msg1", "msg2", "msg3"))
+  println(b) // => WriterT((Vector(msg1, msg2, msg3),123))
+
+  val aResult: Int = a.value
+  println(aResult) // => 123
+
+  val aLog: Vector[String] = a.written
+  println(aLog) // => Vector(msg1, msg2, msg3)
+
+  val (log, result) = b.run
+  println(log) // => Vector(msg1, msg2, msg3)
+  println(result) // => 123
+
+}
